8a2dd958694924685daa85a3ed2144002bde0a46
Joey <48295680+Colbalt01@users.noreply.github.com>
2022-04-04 11:39:50 -0600

    Added Hello World and Week_02, and edited them.

--- Week_02/Employee.cpp
+++ Week_02/Employee.cpp
@@ -1,1 +1,16 @@
 #include "Employee.h"
+
+#include <string.h>
+
+//Constructor
+Employee::Employee() {
+	memset(this->name, '\0', sizeof(this->name));
+}
+//
+void Employee::Read() {
+
+}
+//
+void Employee::Write() {
+
+}
--- Week_02/Employee.h
+++ Week_02/Employee.h
@@ -1,5 +1,20 @@
 #pragma once
 class Employee
 {
+public:
+	const float TAX = 0.01f;
+	char name[31]; // array of char 0 long w/null terminator
+	int age = 0;
+
+protected:
+
+private:
+
+public:
+	Employee();
+	void Read();
+	void Read(int x);
+	void Write();
+
 };
 
